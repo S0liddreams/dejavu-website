138df770df8ae71eb5cf305079931249d1c4bece
Chukwuemeka Uche <chucksilver68@gmail.com>
2025-07-19 22:26:23 +0100

    update on FAQ
    update on FAQ

--- src/components/FrequentQuestions/FrequentQuestions.jsx
+++ src/components/FrequentQuestions/FrequentQuestions.jsx
@@ -27,7 +27,7 @@ const FrequentQuestions = () => {
         </div>
 
         <div className="lg:w-[904px] mt-[24px]  bg-[#FFFFFF] mx-auto rounded-[16px] lg:gap-[8px] p-[24px] lg:p-[40px]">
-        <div className="lg:h-[48px] bg-white gap-4 h-[152px] flex flex-col lg:flex-row justify-between w-full  ">
+        <div className="lg:h-[48px] bg-white gap-4 lg:gap-0 h-[152px] flex flex-col lg:flex-row justify-between w-full  ">
               <div className="leading-[150%] w-[279px] lg:w-[651px] ">
                 <p className="lg:text-[30px] text-[20px] text-[#171717] font-medium lg:h-[36px]">Still have a question?</p>
                 <p className="text-[#737373] mt-[10px]  text-[14px] w-[279px] md:w-[400px] lg:w-[710px]  xl:w-[740px] leading-[150%] ">
@@ -37,7 +37,7 @@ const FrequentQuestions = () => {
 
               {/** Contact Us  */}
 
-              <div className="z-50 xl:w-[150px] lg:w-[150px] w-[134px] h-[44px]  pl-[12px] lg:h-[48px] lg:pl-[12px] lg:pr-[4px] rounded-[100px] flex justify-between items-center text-white font-medium bg-[#0550A1] text-[13px] md:text-[14px] xl:text-[16px] shadow-md hover:shadow-2xl transition-all duration-300 ease-in-out group">
+              <div className="z-50 xl:w-[150px] lg:mt-[20px] lg:w-[150px] w-[134px] h-[44px]  pl-[12px] lg:h-[48px] lg:pl-[12px] lg:pr-[4px] rounded-[100px] flex justify-between items-center text-white font-medium bg-[#0550A1] text-[13px] md:text-[14px] xl:text-[16px] shadow-md hover:shadow-2xl transition-all duration-300 ease-in-out group">
                 Contact Us
                 <Link href="">
                   <div className=" bg-white lg:w-[40px] lg:h-[40px] w-[36px] h-[36px] rounded-full text-[#0550A1] flex justify-center items-center transition-all duration-300 ease-in-out transform group-hover:scale-110 group-hover:bg-black group-hover:shadow-lg">
