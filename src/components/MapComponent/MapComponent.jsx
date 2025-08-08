b36862612a10b62f30a1584514ac8c2b20c3b390
Jedidiah Ogwumike <jedhppc@gmail.com>
2025-08-08 03:55:42 +0100

    Update MapComponent.jsx

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,34 +1,17 @@
 'use client';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import 'leaflet/dist/leaflet.css';
 
 export default function MapComponent() {
   return (
     <div className="w-full h-[500px] rounded-[16px] overflow-hidden">
       <iframe 
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11732.853545800222!2d-89.40902987250419!3d40.63365028815955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880baaaaaaaaaaab%3A0x5346b8f2d30609c9!2scobnbix!5e0!3m2!1sen!2sng!4v1754558309831!5m2!1sen!2sng" 
-        width="600" height="450" style={{ border: 0, width: "100%" }}   
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d186905.63525309195!2d-81.41342914026532!3d42.948762786611184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882ef20ea88d9b0b%3A0x28c7d7699a056b95!2sLondon%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sng!4v1754620369819!5m2!1sen!2sng" 
+        width="800" height="600" style={{ border: 0, width: "100%" }}  
         allowFullScreen="" loading="lazy" 
         referrerPolicy="no-referrer-when-downgrade">
         </iframe>
-      {/* <MapContainer
-        center={[6.5244, 3.3792]} // Example: Lagos
-        zoom={13}
-        scrollWheelZoom={true}
-        style={{ height: '100%', width: '100%' }}
-        
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[6.5244, 3.3792]}>
-          <Popup>
-            A sample marker in Lagos.
-          </Popup>
-        </Marker>
-      </MapContainer> */}
     </div>
   );
 }
